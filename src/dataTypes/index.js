d54a929b3dab909a9c742a9d52dd58a7721987e6
Lord Jerry <lovinjerry004@gmail.com>
2019-11-05 21:28:14 -0800

    implement method for VARCHAR data type

--- src/dataTypes/index.js
+++ src/dataTypes/index.js
@@ -16,6 +16,21 @@ class DataTypes {
 
     return `CHAR(${size})`;
   }
+
+  /**
+   * VARCHAR Holds a variable length string that can contain letters, numbers,
+   * and special characters. Can store up to 255 characters
+   * NOTE: if you put a greater value than 255 it will be converted to a TEXT type
+   * @param { size } size - VARCHAR size
+   * @returns { string }
+   */
+  static VarChar(size = 255) {
+    if (typeof size !== 'number') {
+      throw new Error(`invalid type expected a number but got a ${typeof size} instead`);
+    }
+
+    return `VARCHAR(${size})`;
+  }
 }
 
 module.exports = DataTypes;
