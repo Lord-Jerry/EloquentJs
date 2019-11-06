21c12c664c0c57687d9412131e0abfdaa35614b2
Lord Jerry <lovinjerry004@gmail.com>
2019-11-05 21:20:21 -0800

    implement method for CHAR data type

--- src/dataTypes/index.js
+++ src/dataTypes/index.js
@@ -1,7 +1,21 @@
 
 /**
- * 
+ *
  */
 class DataTypes {
+  /**
+   * CHAR Holds a fixed length string that can contain letters, number and special characters
+   * can store up to 255 characters
+   * @param { integer } size - CHAR size
+   * @returns { string }
+   */
+  static Char(size = 255) {
+    if (typeof size !== 'number') {
+      throw new Error(`invalid type expected a number but got a ${typeof size} instead`);
+    }
 
+    return `CHAR(${size})`;
+  }
 }
+
+module.exports = DataTypes;
