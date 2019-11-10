402ffa207e6ceb60a8a392c12fd25076023f78e4
Lord Jerry <lovinjerry004@gmail.com>
2019-11-09 19:30:05 -0800

    implement method for generating sql types

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const DataTypes = require('./src/dataTypes/index');
 const DataTypesError = require('./src/dataTypes/error');
+const Schema = require('./src/schema/index');
 
 module.exports = {
   DataTypes,
   DataTypesError,
+  Schema,
 };
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,6 +1,16 @@
 const _ = require('lodash');
 const logger = require('../utils/logger');
+const SchemaError = require('./error');
 
+const generateTypes = (options, key) => {
+  const { type } = options;
+
+  if (typeof type !== 'function') {
+    throw new SchemaError(`Invalid Argument, Expected type of ${key} to be a Function But Got ${typeof type} Instead`);
+  }
+
+  return `  ${key} ${type()}`;
+};
 class Schema {
   constructor(tableName, options) {
     this.tableName = tableName;
@@ -8,18 +18,20 @@ class Schema {
     this.sql = '';
 
     if (typeof this.tableName !== 'string') {
-      throw new Error('error');
+      throw new SchemaError('error');
     }
   }
 
   toSql() {
-    this.sql += `CREATE TABLE IF NOT EXISTS ${this.tableName} (`;
+    this.sql += `CREATE TABLE IF NOT EXISTS ${this.tableName} ( \n`;
     _.forEach(this.options, (value, key) => {
-      logger(value.type(), key);
+      this.sql += generateTypes(value, key);
+      this.sql += '\n';
     });
+    this.sql += '\n)';
 
     logger(this.sql);
   }
 }
 
-module.exports = { Schema };
+module.exports = Schema;
