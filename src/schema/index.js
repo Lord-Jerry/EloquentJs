f194782f57d30c8a277d68b0575e44ba8bf39d66
Lord Jerry <lovinjerry004@gmail.com>
2019-11-11 13:59:54 -0800

    create function for generating primary key, and create function for generating column name

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,6 +2,11 @@ const _ = require('lodash');
 const logger = require('../utils/logger');
 
 /**
+ * @param { string } key - column name
+ */
+const generateColumnName = (key) => ` ${key}`;
+
+/**
  * generate SQL type from provided options
  * @param { object } options -
  * @param { string} key - column name
@@ -14,7 +19,7 @@ const generateTypes = (options, key) => {
     throw new TypeError(`Invalid Argument, Expected type of ${key} to be a Function But Got ${typeof type} Instead`);
   }
 
-  return `  ${key}  ${type()}`;
+  return `  ${type()}`;
 };
 
 /**
@@ -34,6 +39,21 @@ const generateNull = (options, key) => {
   return allowNull ? ' NULL' : ' NOT NULL';
 };
 
+/**
+ * @param { object } options
+ * @param { string } key - column name
+ */
+const generatePrimaryKey = (options, key) => {
+  let { primaryKey } = options;
+
+  if (primaryKey === undefined) primaryKey = false;
+
+  if (typeof primaryKey !== 'boolean') {
+    throw new TypeError(`Invalid Argument, Expected primaryKey of ${key} to be a Boolean, But Got ${typeof type} Instead`);
+  }
+
+  return primaryKey ? ' PRIMARY KEY' : '';
+};
 class Schema {
   constructor(tableName, options) {
     if (typeof tableName !== 'string') {
@@ -48,8 +68,10 @@ class Schema {
   toSql() {
     this.sql += `CREATE TABLE IF NOT EXISTS ${this.tableName} ( \n`;
     _.forEach(this.options, (value, key) => {
+      this.sql += generateColumnName(key);
       this.sql += generateTypes(value, key);
       this.sql += generateNull(value, key);
+      this.sql += generatePrimaryKey(value, key);
       this.sql += ',\n';
     });
     this.sql += '\n)';
