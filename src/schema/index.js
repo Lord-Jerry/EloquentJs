61f1d4b20287d5e4c2364ebb3758fff6b54591b4
Lord Jerry <lovinjerry004@gmail.com>
2019-11-11 13:49:51 -0800

    implement function for creating SQL NULL and NOT NULL values

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,6 +1,12 @@
 const _ = require('lodash');
 const logger = require('../utils/logger');
 
+/**
+ * generate SQL type from provided options
+ * @param { object } options -
+ * @param { string} key - column name
+ * @returns { string || TypeError }
+ */
 const generateTypes = (options, key) => {
   const { type } = options;
 
@@ -10,6 +16,24 @@ const generateTypes = (options, key) => {
 
   return `  ${key}  ${type()}`;
 };
+
+/**
+ * @param { object } options -
+ * @param { string } - key
+ * @returns { string }
+ */
+const generateNull = (options, key) => {
+  let { allowNull } = options;
+
+  if (allowNull === undefined) allowNull = true;
+
+  if (typeof allowNull !== 'boolean') {
+    throw new TypeError(`Invalid Argument, Expected allowNull of ${key} to be a Boolean, But Got ${typeof type} Instead`);
+  }
+
+  return allowNull ? ' NULL' : ' NOT NULL';
+};
+
 class Schema {
   constructor(tableName, options) {
     if (typeof tableName !== 'string') {
@@ -25,6 +49,7 @@ class Schema {
     this.sql += `CREATE TABLE IF NOT EXISTS ${this.tableName} ( \n`;
     _.forEach(this.options, (value, key) => {
       this.sql += generateTypes(value, key);
+      this.sql += generateNull(value, key);
       this.sql += ',\n';
     });
     this.sql += '\n)';
